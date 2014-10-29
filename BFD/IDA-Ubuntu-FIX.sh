1747f450861797ad1d4f9f68f72d0d508e8c332b
Dhiru Kholia <dhiru@spotify.com>
2014-10-29 23:03:42 +0100

    BFD stuff

---
+++ BFD/IDA-Ubuntu-FIX.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo aptitude install libstdc++6:i386 libglib2.0-0:i386 libfreetype6:i386 libfontconfig1:i386 libXrender1:i386 libsm6:i386 libfreetype6:i386 libglib2.0-0:i386 libxext6:i386 -y
